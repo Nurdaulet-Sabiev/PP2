dfed262012a45747ef315d0642b05e9559e77ac0
Nurdaulet Sabiev <nurdasabiev@gmail.com>
2026-02-02 12:32:49 +0500

    Add files via upload

---
+++ week3/w3_school/class.py
@@ -0,0 +1,56 @@
+class MyClass:
+  x = 5
+
+
+
+p1 = MyClass()
+print(p1.x)
+
+
+
+del p1
+
+
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 28)
+
+print(p1.name)
+print(p1.age)
+
+
+class Person:
+  def __init__(myobject, name, age):
+    myobject.name = name
+    myobject.age = age
+
+  def greet(abc):
+    print("Hello, my name is " + abc.name)
+
+p1 = Person("Emil", 36)
+p1.greet()
